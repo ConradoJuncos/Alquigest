6f816e5b6b2c4faab0833aa68af099bdd1ba3ddc
Conrado <conryjuncon@hotmail.com>
2025-09-21 10:49:25 -0300

    No habia hecho add de inquilino.ts

--- frontend/types/Inquilino.ts
+++ frontend/types/Inquilino.ts
@@ -2,7 +2,7 @@ export interface Inquilino {
   id: number;
   nombre: string;
   apellido: string;
-  cuil: string;
-  telefono: string;
+  cuil?: string;  // Optional since it can be null in the DER
+  telefono?: string;  // Optional since it can be null in the DER
   esActivo: boolean;
 }
