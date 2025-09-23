f95dd4fba9d455565996a25bdcb53d7eba0a07e3
Conrado <conryjuncon@hotmail.com>
2025-09-22 22:30:16 -0300

    update notas, cambio de titulos en swagger

--- backend/src/main/java/com/alquileres/controller/InmuebleController.java
+++ backend/src/main/java/com/alquileres/controller/InmuebleController.java
@@ -7,11 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/inmuebles")
+@Tag(name = "Inmuebles", description = "API para gestión de inmuebles")
 public class InmuebleController {
 
     @Autowired
