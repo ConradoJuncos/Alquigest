6cd9879563a9527205419ccfbb3e8a957bfdd513
MauroGenesir <90090@sistemas.frc.utn.edu.ar>
2025-10-22 11:24:09 -0300

    Debug base de datos

--- backend/src/main/java/com/alquileres/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/alquileres/exception/GlobalExceptionHandler.java
@@ -2,6 +2,10 @@ package com.alquileres.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
